15bf666434a49769a431d2d2d2c52b200aaf78d1
yjkim <chitose_@naver.com>
2021-06-01 12:35:35 +0900

    회원상세정보관리 impl 작성중

---
+++ src/main/java/proj21_shoes/service/impl/GetMemberDetailServiceImpl.java
@@ -0,0 +1,14 @@
+package proj21_shoes.service.impl;
+
+import proj21_shoes.dto.MemberDetail;
+import proj21_shoes.service.GetMemberDetailService;
+
+public class GetMemberDetailServiceImpl implements GetMemberDetailService {
+
+	@Override
+	public MemberDetail getMemberDetail(long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/main/java/proj21_shoes/service/impl/RegisterMemberDetailServiceImpl.java
@@ -0,0 +1,14 @@
+package proj21_shoes.service.impl;
+
+import proj21_shoes.dto.MemberDetail;
+import proj21_shoes.service.RegisterMemberDetailService;
+
+public class RegisterMemberDetailServiceImpl implements RegisterMemberDetailService {
+
+	@Override
+	public int registerMemberDetail(MemberDetail memberDetail) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
