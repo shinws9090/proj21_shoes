9c8c577fcbe9a7dee4ab11f8d45f5e5255c14f11
yjkim <chitose_@naver.com>
2021-06-01 12:35:57 +0900

    수정

--- src/main/java/proj21_shoes/service/impl/RemoveMemberDetailServiceImpl.java
+++ src/main/java/proj21_shoes/service/impl/RemoveMemberDetailServiceImpl.java
@@ -1,6 +1,6 @@
 package proj21_shoes.service.impl;
 
-public class RemoveMemberDetailService implements proj21_shoes.service.RemoveMemberDetailService {
+public class RemoveMemberDetailServiceImpl implements proj21_shoes.service.RemoveMemberDetailService {
 
 	@Override
 	public int removeMemberDetail(long id) {
