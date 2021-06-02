c3f3ce36661267647666c422d0e94020ad5762b2
yjkim <chitose_@naver.com>
2021-06-02 16:31:23 +0900

    패키지 추가.exceptionhandler

---
+++ src/main/java/proj21_shoes/common/CommonExceptionHandler.java
@@ -0,0 +1,11 @@
+package proj21_shoes.common;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice("proj21_shoes")//이 프로젝트 모든 패키지에 있는 익셉션 가져다쓸수있당!
+public class CommonExceptionHandler {
+	
+
+
+}
