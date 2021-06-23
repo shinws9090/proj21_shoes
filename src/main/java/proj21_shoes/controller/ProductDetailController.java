a4fdad0db7290d8fcdb8569306b15f1d07584065
이민정 <rukiminangel@naver.com>
2021-06-23 16:13:58 +0900

    충돌 수정

--- src/main/java/proj21_shoes/controller/ProductDetailController.java
+++ src/main/java/proj21_shoes/controller/ProductDetailController.java
@@ -2,10 +2,8 @@ package proj21_shoes.controller;
 
 import java.util.List;
 
-
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -18,13 +16,8 @@ import org.springframework.web.servlet.ModelAndView;
 import proj21_shoes.commend.MyPageSelectCommend;
 import proj21_shoes.commend.MyQnaViewCommand;
 import proj21_shoes.dto.Product;
-
-import proj21_shoes.dto.ReView;
-import proj21_shoes.review.ReviewService;
-
 import proj21_shoes.service.MyQnaService;
 import proj21_shoes.service.ProductQnaService;
-
 import proj21_shoes.service.ProductService;
 
 @RestController
@@ -32,30 +25,14 @@ public class ProductDetailController {
 	
 	@Autowired
 	ProductService service;
-
-	
-	@Autowired
-	ReviewService rService;
-
 	@Autowired
 	private ProductQnaService qService;
-
 
 	@GetMapping("/productDetail/{code}")
 	public ModelAndView productDetail(@PathVariable("code")int code) {
 		Product product = service.productByCode(code);
-
-		List<ReView> reViewList = rService.selectReviewByProductCode(code);
-		System.out.println(reViewList);
-		
-		ModelAndView mav = new ModelAndView("product/productDetail","product",product);
-		mav.addObject("reviewList",reViewList);
-		
-		return mav;
-
 		List<MyQnaViewCommand> qnaList = qService.selectProductQnAbyCode(code);
 		return new ModelAndView("product/productDetail","product",product).addObject("myQnAList",qnaList);
-
 	}
 	
 	@GetMapping("api/size")
